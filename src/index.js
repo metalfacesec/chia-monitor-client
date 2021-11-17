977f7e8cc379abc39c14c9466ff7c7cb574859d9
Git <metalfacesec@gmail.com>
2021-11-16 22:48:46 -0500

    Adding system memory logging

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const os = require('os');
 const axios = require('axios');
 
 const serverIP = '127.0.0.1';
@@ -14,4 +15,27 @@ function sendHeartbeat() {
 	});
 }
 
-setInterval(sendHeartbeat, 5000)
+function sendSystemStats() {
+	let systemStats = {
+		total_memory: os.totalmem(),
+		free_memory: os.freemem()
+	};
+	console.log('Calling log system stats');
+	axios.post(`http://${serverIP}:${serverPort}/log-system-stats`, systemStats)
+	.then(response => {
+		console.log(response.data);
+	})
+	.catch(err => {
+		console.log('Err: ' + err);
+	});
+}
+
+function init() {
+	setInterval(sendHeartbeat, 30000);
+	sendHeartbeat();
+
+	setInterval(sendSystemStats, 5000);
+	sendSystemStats();
+}
+
+init();
