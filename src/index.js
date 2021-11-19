46dd67e165b4215f82a36d9dacf07aa674b96f61
Git <metalfacesec@gmail.com>
2021-11-18 19:41:43 -0500

    Adding prototype for pulling blockchain size

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const os = require('os');
 const axios = require('axios');
+const exec = require('child_process').exec;
 
 const serverIP = '127.0.0.1';
 const serverPort = '8081';
@@ -30,12 +31,32 @@ function sendSystemStats() {
 	});
 }
 
+function getBlockchainSize() {
+	console.log('Calling log blockchain size');
+
+	let command = 'du -s /home/metalface/.chia/mainnet/db';
+    exec(command, (error, stdout, stderr) => {
+		let split = stdout.split('\t');
+
+		axios.get(`http://${serverIP}:${serverPort}/log-blockchain-size?size=${split[0]}`)
+		.then(response => {
+			console.log(response.data);
+		})
+		.catch(err => {
+			console.log('Err: ' + err);
+		});
+	});
+};
+
 function init() {
 	setInterval(sendHeartbeat, 30000);
 	sendHeartbeat();
 
 	setInterval(sendSystemStats, 5000);
 	sendSystemStats();
+
+	setInterval(getBlockchainSize, 60000);
+	getBlockchainSize();
 }
 
 init();
