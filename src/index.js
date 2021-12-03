3dd1013ccd62364ecfb3a11143c4e19b92698b8a
Git <metalfacesec@gmail.com>
2021-12-02 20:37:57 -0500

    Swapping functions out for chia npm lib

--- src/index.js
+++ src/index.js
@@ -1,159 +1,46 @@
+const { Wallet } = require('chia-javascript');
+
 const os = require('os');
-const fs = require('fs');
 const axios = require('axios');
-const https = require('https');
-const exec = require('child_process').exec;
 
-const config = require('../config/config.json');
-
-const serverIP = '127.0.0.1';
+const serverIP = '192.168.0.199';
 const serverPort = '8081';
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-function getPrivateKeys(fingerprint) {
-	return new Promise((resolve, reject) => {
-		let postData = JSON.stringify({fingerprint: fingerprint});
+let wallet = new Wallet();
 
-		let options = {
-			key: fs.readFileSync(config.wallet_private_key),
-			cert: fs.readFileSync(config.wallet_private_crt),
-			hostname: config.chia_rpc_ip,
-			port: config.chia_rpc_port,
-			path: '/get_private_key',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				'Content-Length': postData.length
-			}
-		};
-
-		var req = https.request(options, (res) => {
-			res.on('data', d => {
-				let response = JSON.parse(d);
-				if (response.success === true) {
-					return resolve(response);
-				}
-				reject('Invalid response format: ' + d);
-			});
-		});
-
-		req.on('error', (e) => {
-			reject(e);
-		});
-
-		req.write(postData);
-		req.end();
-	});
+async function getPublicKeys() {
+	let publicKeys = await wallet.getPublicKeys();
+	if (publicKeys === null || typeof publicKeys !== 'object' || !publicKeys.success) {
+		return [];
+	}
+	return publicKeys.public_key_fingerprints;
 }
 
-function getPublicKeys() {
-	return new Promise((resolve, reject) => {
-		let postData = JSON.stringify({});
+async function getWallets() {
+	let walletResponse = await wallet.getWallets();
 
-		let options = {
-			key: fs.readFileSync(config.wallet_private_key),
-			cert: fs.readFileSync(config.wallet_private_crt),
-			hostname: config.chia_rpc_ip,
-			port: config.chia_rpc_port,
-			path: '/get_public_keys',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				'Content-Length': postData.length
-			}
-		};
+	if (walletResponse === null || typeof walletResponse !== 'object') {
+		console.log('Get wallets invalid reponse format');
+		return [];
+	}
 
-		var req = https.request(options, (res) => {
-			res.on('data', d => {
-				let response = JSON.parse(d);
-				if (response.success === true) {
-					return resolve(response);
-				}
-				reject('Invalid response format: ' + d);
-			});
-		});
+	if (!walletResponse.success) {
+		console.log('Get wallets response success false');
+		return [];
+	}
 
-		req.on('error', (e) => {
-			reject(e);
-		});
+	if (!walletResponse.wallets === null || typeof walletResponse.wallets !== 'object') {
+		console.log('Get wallets response has no wallets key');
+		return [];
+	}
 
-		req.write(postData);
-		req.end();
-	});
-}
-
-function getWalletBalance(walletId) {
-	return new Promise((resolve, reject) => {
-		let postData = JSON.stringify({
-			wallet_id: walletId
-		});
-
-		let options = {
-			key: fs.readFileSync(config.wallet_private_key),
-			cert: fs.readFileSync(config.wallet_private_crt),
-			hostname: config.chia_rpc_ip,
-			port: config.chia_rpc_port,
-			path: '/get_wallet_balance',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				'Content-Length': postData.length
-			}
-		};
-
-		var req = https.request(options, (res) => {
-			res.on('data', d => {
-				let response = JSON.parse(d);
-				if (response.success === true && response.wallet_balance !== null && typeof response.wallet_balance === 'object') {
-					return resolve(response.wallet_balance);
-				}
-				reject('Invalid response format from get wallet balance: ' + d);
-			});
-		});
-
-		req.on('error', (e) => {
-			reject(e);
-		});
-
-		req.write(postData);
-		req.end();
-	});
-}
-
-function getWallets() {
-	return new Promise((resolve, reject) => {
-		let postData = JSON.stringify({});
-
-		let options = {
-			key: fs.readFileSync(config.wallet_private_key),
-			cert: fs.readFileSync(config.wallet_private_crt),
-			hostname: config.chia_rpc_ip,
-			port: config.chia_rpc_port,
-			path: '/get_wallets',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				'Content-Length': postData.length
-			}
-		};
-
-		var req = https.request(options, (res) => {
-			res.on('data', d => {
-				let response = JSON.parse(d);
-				if (response.success === true && response.wallets !== null && typeof response.wallets === 'object' && response.wallets.length) {
-					resolve(response.wallets);
-				}
-			});
-		});
-
-		req.on('error', (e) => {
-			reject(e);
-		});
-
-		req.write(postData);
-		req.end();
-	});
+	let walletBalances = [];
+	for (let i = 0; i < walletResponse.wallets.length; i++) {
+		let balanceInfo = await wallet.getWalletBalance(walletResponse.wallets[i].id);
+		walletBalances.push(balanceInfo.wallet_balance);
+	}
 }
 
 function sendHeartbeat() {
@@ -182,23 +69,6 @@ function sendSystemStats() {
 	});
 }
 
-function getBlockchainSize() {
-	console.log('Calling log blockchain size');
-
-	let command = 'du -s /home/metalface/.chia/mainnet/db';
-    exec(command, (error, stdout, stderr) => {
-		let split = stdout.split('\t');
-
-		axios.get(`http://${serverIP}:${serverPort}/log-blockchain-size?size=${split[0]}`)
-		.then(response => {
-			console.log(response.data);
-		})
-		.catch(err => {
-			console.log('Err: ' + err);
-		});
-	});
-};
-
 async function init() {
 	setInterval(sendHeartbeat, 30000);
 	sendHeartbeat();
@@ -206,30 +76,13 @@ async function init() {
 	setInterval(sendSystemStats, 5000);
 	sendSystemStats();
 
-	setInterval(getBlockchainSize, 60000);
-	getBlockchainSize();
+	setInterval(getWallets, 10000);
+	getWallets();
 
-	if (config.main_node) {
-		try {
-			let publicKeys = await getPublicKeys();
-			publicKeys.public_key_fingerprints.forEach(async fingerprint => {
-				let privateKeys = await getPrivateKeys(fingerprint);
-				console.log(privateKeys);
-			});
-			
-			let wallets = await getWallets();
-			wallets.forEach(async wallet => {
-				try {
-					let walletBalance = await getWalletBalance(wallet.id);
-					console.log(walletBalance);
-				} catch (err) {
-					console.log(err);
-				}
-			});
-		} catch (err) {
-			console.log(err);
-		}
-		
+	let publicKeys = await getPublicKeys();
+	if (publicKeys.length) {
+		let privateKeys = await wallet.getPrivateKey(publicKeys[0]);
+		console.log(privateKeys)
 	}
 }
 
